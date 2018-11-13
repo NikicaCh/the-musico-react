9946fc5efa1f31a9007deca2f3e35a8d8ad295a8
unknown <nikica_chelsea@hotmail.com>
2018-11-13 11:22:29 +0100

    process front

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -431,7 +431,7 @@ class Player extends Component {
 
         player.addListener('initialization_error', ({ message }) => { console.error(message); });
         player.addListener('authentication_error', ({ message }) => {
-                window.location.replace(`http://localhost:3001/login`)
+                window.location.replace(`http://localhost:${process.env.PORT}/login`)
             }
         );
         player.addListener('account_error', ({ message }) => { console.error("MESSAGE",message); });
